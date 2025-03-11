850d6892c7a18eead257c61d039b120b69ff2547
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-03-11 11:04:01 -0500

    refactored Auth files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+import { Authorized } from "./views/Authorized"
+import { Login } from "./components/auth/Login"
+import { Register } from "./components/auth/Register"
+
 function App() {
 
   return (
-    <>
-     
-    </>
+    <Routes>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="*" element={
+        <Authorized/>
+      }/>
+    </Routes>
   )
 }
 
